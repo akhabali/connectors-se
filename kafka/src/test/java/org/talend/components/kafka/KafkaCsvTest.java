a37d4c49e72c1b1f5b9170cf3e815771ae0797b5
Chen Bin <bchen@talend.com>
2018-11-06 16:27:20 +0800

    enrich csv unit test

--- kafka/src/test/java/org/talend/components/kafka/KafkaCsvTest.java
+++ kafka/src/test/java/org/talend/components/kafka/KafkaCsvTest.java
@@ -33,7 +33,7 @@ import net.manub.embeddedkafka.EmbeddedKafka$;
 import net.manub.embeddedkafka.EmbeddedKafkaConfig$;
 import scala.collection.Map$;
 
-public class KafkaInputTest {
+public class KafkaCsvTest {
 
     KafkaConnectionConfiguration datastoreProperties;
 
@@ -62,12 +62,10 @@ public class KafkaInputTest {
 
     @Before
     public void setup() {
-        EmbeddedK brk = EmbeddedKafka$.MODULE$
-                .start(EmbeddedKafkaConfig$.MODULE$
-                        .apply(0, 0, //
-                                Map$.MODULE$.<String, String> empty(), //
-                                Map$.MODULE$.<String, String> empty(), //
-                                Map$.MODULE$.<String, String> empty()));
+        EmbeddedK brk = EmbeddedKafka$.MODULE$.start(EmbeddedKafkaConfig$.MODULE$.apply(0, 0, //
+                Map$.MODULE$.<String, String> empty(), //
+                Map$.MODULE$.<String, String> empty(), //
+                Map$.MODULE$.<String, String> empty()));
         broker = "localhost:" + brk.config().kafkaPort();
         zookeeper = "localhost:" + brk.config().zooKeeperPort();
 
@@ -95,14 +93,58 @@ public class KafkaInputTest {
      * using standard field delimiter
      */
     @Test
-    public void csvBasicTest() {
+    public void csvBasicTest1() {
         inputDatasetProperties.setFieldDelimiter(FieldDelimiterType.SEMICOLON);
         outputDatasetProperties.setFieldDelimiter(FieldDelimiterType.SEMICOLON);
 
-        basicTest("csvBasicTest", "1", fieldDelimiter);
+        testCase1("csvBasicTest1", "1", fieldDelimiter);
     }
 
-    public void basicTest(String title, String topicSuffix, String fieldDelim) {
+    /**
+     * Read csv format value and write csv format value without schema and do not write key
+     * using standard field delimiter
+     */
+    @Test
+    public void csvBasicTest2() {
+        inputDatasetProperties.setFieldDelimiter(FieldDelimiterType.SEMICOLON);
+        outputDatasetProperties.setFieldDelimiter(FieldDelimiterType.SEMICOLON);
+
+        testCase2("csvBasicTest2", "2", fieldDelimiter);
+    }
+
+    /**
+     * Read csv format value and write csv format value without schema and do not write key
+     * using custom field delimiter
+     */
+    @Test
+    public void csvBasicTest1WithOtherFieldDelimiter() {
+        inputDatasetProperties.setFieldDelimiter(FieldDelimiterType.OTHER);
+        inputDatasetProperties.setSpecificFieldDelimiter(otherFieldDelimiter);
+        outputDatasetProperties.setFieldDelimiter(FieldDelimiterType.OTHER);
+        outputDatasetProperties.setSpecificFieldDelimiter(otherFieldDelimiter);
+
+        testCase1("csvBasicTest1WithOtherFieldDelimiter", "3", otherFieldDelimiter);
+    }
+
+    /**
+     * Read csv format value and write csv format value without schema and do not write key
+     * using standard field delimiter
+     */
+    @Test
+    public void csvBasicTest2WithOtherFieldDelimiter() {
+        inputDatasetProperties.setFieldDelimiter(FieldDelimiterType.OTHER);
+        inputDatasetProperties.setSpecificFieldDelimiter(otherFieldDelimiter);
+        outputDatasetProperties.setFieldDelimiter(FieldDelimiterType.OTHER);
+        outputDatasetProperties.setSpecificFieldDelimiter(otherFieldDelimiter);
+
+        testCase2("csvBasicTest2WithOtherFieldDelimiter", "4", otherFieldDelimiter);
+    }
+
+    /**
+     * Input with earliest & maxRecords
+     * Output with round_robin partition & Non Compress
+     */
+    public void testCase1(String title, String topicSuffix, String fieldDelim) {
         String testID = title + new Random().nextInt();
 
         expectedPersons = Person.genRandomList(testID, maxRecords);
@@ -115,8 +157,7 @@ public class KafkaInputTest {
 
         Producer<Void, String> producer = new KafkaProducer<>(props);
         for (Person person : expectedPersons) {
-            ProducerRecord<Void, String> message =
-                    new ProducerRecord<>(topicIn + topicSuffix, person.toCSV(fieldDelim));
+            ProducerRecord<Void, String> message = new ProducerRecord<>(topicIn + topicSuffix, person.toCSV(fieldDelim));
             producer.send(message);
         }
         producer.close();
@@ -125,8 +166,8 @@ public class KafkaInputTest {
         KafkaInputConfiguration inputProperties = new KafkaInputConfiguration();
         inputProperties.setDataset(inputDatasetProperties);
         inputProperties.setAutoOffsetReset(OffsetResetStrategy.EARLIEST);
-        inputProperties.setUseMaxReadTime(true);
-        inputProperties.setMaxReadTime(5000l);
+        inputProperties.setUseMaxNumRecords(true);
+        inputProperties.setMaxNumRecords(Long.valueOf(maxRecords));
 
         KafkaOutputConfiguration outputProperties = new KafkaOutputConfiguration();
         outputProperties.setDataset(outputDatasetProperties);
@@ -173,6 +214,91 @@ public class KafkaInputTest {
         // ----------------- Read data from TOPIC_OUT end --------------------
 
         assertEquals(expectedPersons, results);
+    }
+
+    /**
+     * Input with earliest & maxTime 5s
+     * Output with by_key partition & Non Compress
+     */
+    public void testCase2(String title, String topicSuffix, String fieldDelim) {
+        String testID = title + new Random().nextInt();
+
+        expectedPersons = Person.genRandomList(testID, maxRecords);
+
+        // ----------------- Send data to TOPIC_IN start --------------------
+        Properties props = new Properties();
+        props.put("bootstrap.servers", broker);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<Void, String> producer = new KafkaProducer<>(props);
+        for (Person person : expectedPersons) {
+            ProducerRecord<Void, String> message = new ProducerRecord<>(topicIn + topicSuffix, person.toCSV(fieldDelim));
+            producer.send(message);
+        }
+        producer.close();
+        // ----------------- Send data to TOPIC_IN done --------------------
+
+        KafkaInputConfiguration inputProperties = new KafkaInputConfiguration();
+        inputProperties.setDataset(inputDatasetProperties);
+        inputProperties.setAutoOffsetReset(OffsetResetStrategy.EARLIEST);
+        inputProperties.setUseMaxReadTime(true);
+        inputProperties.setMaxReadTime(5000l);
+
+        KafkaOutputConfiguration outputProperties = new KafkaOutputConfiguration();
+        outputProperties.setDataset(outputDatasetProperties);
+        outputProperties.setPartitionType(KafkaOutputConfiguration.PartitionType.COLUMN);
+        outputProperties.setKeyColumn("field1");
+
+        inputDatasetProperties.setTopic(topicIn + topicSuffix);
+        outputDatasetProperties.setTopic(topicOut + topicSuffix);
+
+        KafkaInput inputRuntime = new KafkaInput(inputProperties);
+        KafkaOutput outputRuntime = new KafkaOutput(outputProperties);
+
+        // ----------------- pipeline start --------------------
+        pipeline.apply(inputRuntime).apply(Filter.by(new FilterByGroup(testID))).apply(outputRuntime);
+
+        // TODO: Remove this when tacokit coders are immutable.
+        DirectOptions options = pipeline.getOptions().as(DirectOptions.class);
+        options.setEnforceImmutability(false);
+
+        PipelineResult result = pipeline.run(options);
+        result.waitUntilFinish();
+        // ----------------- pipeline done --------------------
+
+        // ----------------- Read data from TOPIC_OUT start --------------------
+        props = new Properties();
+        props.put("bootstrap.servers", broker);
+        props.put("group.id", testID);
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.offset.reset", "earliest");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(topicOut + topicSuffix));
+        List<Person> results = new ArrayList<>();
+        List<String> keys = new ArrayList<>();
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                Person person = Person.fromCSV(record.value(), fieldDelim);
+                if (testID.equals(person.group)) {
+                    keys.add(record.key());
+                    results.add(person);
+                }
+            }
+            if (results.size() >= maxRecords) {
+                break;
+            }
+        }
+        // ----------------- Read data from TOPIC_OUT end --------------------
+
+        assertEquals(expectedPersons, results);
+        List<String> expectedKeys = new ArrayList<>();
+        for (Person person : results) {
+            expectedKeys.add(person.name);
+        }
+        assertEquals(expectedKeys, keys);
     }
 
     public static class FilterByGroup implements SerializableFunction<Record, Boolean> {
